f540e576baa47838bbda26a846a5d80fe6fdec68
Priyanka <priyanka.pandey@modaoperandi.com>
2024-04-15 21:48:47 +0530

    Handle error cases

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -3,6 +3,8 @@ module Calculator
 
   def self.add(input)
     integers = input.split(/[^0-9-]+/).map(&:to_i)
+    negative_numbers = integers.select(&:negative?)
+    raise "negative numbers not allowed #{negative_numbers.join(', ')}" if negative_numbers.length > 0
     integers.sum
   end
 
